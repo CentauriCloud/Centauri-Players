ab1e8157145b11d64e358d326b05d3a8d49bd4cc
Microsamp <germancraft42@gmail.com>
2017-12-04 16:24:52 +0100

    Better player join handling

--- centauricloud-players-module/src/main/java/org/centauri/cloud/centauricloud/players/module/listener/NetworkHandler.java
+++ centauricloud-players-module/src/main/java/org/centauri/cloud/centauricloud/players/module/listener/NetworkHandler.java
@@ -11,19 +11,25 @@ import org.centauri.cloud.players.common.packets.PacketPlayerDisconnect;
 import org.centauri.cloud.players.common.packets.PacketPlayerJoin;
 
 public class NetworkHandler {
-	
+
 	@Listener
 	public void onReceivingPacket(PacketReceivingEvent event) {
 		Packet packet = event.getPacket();
 		Server server = event.getServer();
-	
-		if(packet instanceof PacketPlayerJoin) {
+
+		if (packet instanceof PacketPlayerJoin) {
 			PacketPlayerJoin joinPacket = (PacketPlayerJoin) packet;
-			if(server instanceof SpigotServer)
+			Player player = CentauriCloudPlayers.getInstance().getPlayerManager().getPlayer(joinPacket.getUniqueId());
+			if (player == null) {
+				player = new Player(joinPacket.getUniqueId(), joinPacket.getName());
+				CentauriCloudPlayers.getInstance().getPlayerManager().add(player);
+				return;
+			}
+
+			if (server instanceof SpigotServer)
 				CentauriCloudPlayers.getInstance().getPlayerManager().getPlayer(joinPacket.getUniqueId()).setServer((SpigotServer) server);
-			else
-				CentauriCloudPlayers.getInstance().getPlayerManager().add(new Player(joinPacket.getUniqueId(), joinPacket.getName()));
-		} else if(packet instanceof PacketPlayerDisconnect) {
+
+		} else if (packet instanceof PacketPlayerDisconnect) {
 			PacketPlayerDisconnect disconnectPacket = (PacketPlayerDisconnect) packet;
 			CentauriCloudPlayers.getInstance().getPlayerManager().remove(disconnectPacket.getUniqueId());
 		}
